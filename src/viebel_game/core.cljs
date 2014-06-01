9effc253d4d9952a6e340a4803349d741054af2e
Raphael Boukara <raphael.boukara@gmail.com>
2014-06-01 22:45:49 +0300

    add timeout

--- src/viebel_game/core.cljs
+++ src/viebel_game/core.cljs
@@ -4,18 +4,11 @@
 	           [gyr.core :only [def.module 
                              	def.controller
                               	def.directive]])
-	(:require [cljs.core.async :refer [<! chan put!]]))
+	(:require [cljs.core.async :refer [<! chan put! timeout]]))
 
 (enable-console-print!)
 
-
-
 (def.module app [ngTouch ngAnimate])
-
-
-
-
-
 
 (def user-input-chan (chan))
 
@@ -44,38 +37,33 @@
 (defn init-watchers [$scope]
 	(add-watch item-val :on-item-val-change (partial on-item-val-change $scope))
  	(add-watch item-count :on-item-count-change (partial on-item-count-change $scope))
-  	(add-watch item-score :on-item-score-change (partial on-item-score-change $scope))
-    (add-watch item-timer :on-item-timer-change (partial on-item-timer-change $scope))
-    (add-watch status-running :on-status-running-change (partial on-status-running-change $scope))
+	(add-watch item-score :on-item-score-change (partial on-item-score-change $scope))
+  (add-watch item-timer :on-item-timer-change (partial on-item-timer-change $scope))
+  (add-watch status-running :on-status-running-change (partial on-status-running-change $scope))
 	(!> $scope.$on "$destroy" (fn[]
 	                            (remove-watch item-val :on-item-val-change)
                              	(remove-watch item-count :on-item-count-change)
-                              	(remove-watch item-score :on-item-score-change)
-                               	(remove-watch item-timer :on-item-score-change)
-                                (remove-watch status-running :on-status-running-change))))
+                            	(remove-watch item-score :on-item-score-change)
+                             	(remove-watch item-timer :on-item-score-change)
+                              (remove-watch status-running :on-status-running-change))))
 
 (defn init-game [] 
    	(reset! item-timer 30))
 
-(defn timeout [$timeout]
-  (let [c (chan)]
-    ($timeout #(put! c :tic) 1000)
-    c))
-
-(defn start-timer [$timeout]
+(defn start-timer []
 	(go-loop [timer 30]
         (reset! item-timer timer)
-		(<! (timeout $timeout))
+		(<! (timeout 1000))
 		(if (= timer 0)
 			(put! user-input-chan :end)
 			(recur (dec timer)))))
   
-(defn run-game [$timeout]
+(defn run-game []
   (go-loop [item-past nil]
       (reset! item-val (rand-nth [2 4 6]))
       (case (<! user-input-chan)
         :start (do
-                 (start-timer $timeout)
+                 (start-timer)
                  (reset! status-running true)
                  (reset! item-count 0)
                  (reset! item-score 0)
@@ -92,9 +80,9 @@
               (swap! item-count inc)
               (recur @item-val)))))
 
-(defn init-model [$scope $timeout]
-  	(init-game)
-	(run-game $timeout))
+(defn init-model []
+  (init-game)
+	(run-game))
 
 (defn init-handlers [$scope]
   (! $scope.game #(put! user-input-chan :start))
@@ -103,7 +91,7 @@
 
 (def.controller app.MainCtrl [$scope $timeout]
   (init-watchers $scope)
-  (init-model $scope $timeout)
+  (init-model)
   (init-handlers $scope))
 
 
